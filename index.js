95c1bd946a9ee83e7980ce4f440458fbacd2cec7
fibonacci <618c@gmx.com>
2021-06-11 16:54:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-function increment() {
-  console.log("button")
-}
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+let count = 0
+
+function increment() 
+  {count += 1
+  countEl.textContent = count}
+
+
+function save() 
+  {let countStr = count + " - "
+  saveEl.textContent += countStr
+  countEl.textContent = 0
+  count = 0}
